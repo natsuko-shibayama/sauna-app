2a51de4e8ec28f9841e9388810c0f14b245970b1
natsuko-shibayama <sn1027.0629@gmail.com>
2024-09-14 12:44:27 +0900

    [fix]管理者ログイン・ログアウト機能実装完了

--- resources/views/components/admin/header.blade.php
+++ resources/views/components/admin/header.blade.php
@@ -4,10 +4,16 @@
         <img src="{{ asset('storage/images/headerLogo.png') }}" alt="Logo" class="w-10 h-10 p-2 bg-white rounded-full border-white shadow-md animate-bounce">
         <span class="ml-3 text-xl text-gray-900">サウナアプリ管理画面</span>
         </a>
-        <button class="inline-flex items-center bg-green-600 text-white border-0 py-1 px-3 focus:outline-none hover:bg-green-700 rounded text-base mt-4 md:mt-0 md:ml-auto">ログアウト
-        {{-- <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-        </svg> --}}
-        </button>
+
+        <form method="POST" action="{{ route('logout') }}" class=" md:ml-auto">
+            @csrf
+            <button
+                href="route('logout')"
+                onclick="event.preventDefault();
+                            this.closest('form').submit();"
+                class="inline-flex items-center bg-green-600 text-white border-0 py-1 px-3 focus:outline-none hover:bg-green-700 rounded text-base mt-4 md:mt-0"
+                >ログアウト
+            </button>
+        </form>
     </div>
 </header>
