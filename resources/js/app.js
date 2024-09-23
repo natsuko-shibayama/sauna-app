3333940521c0e528e0c821c08d09e1b29ac41d0b
natsuko-shibayama <sn1027.0629@gmail.com>
2024-09-23 17:56:10 +0900

    [wip]サウナ施設・サウナの更新処理、バリデーションまで。サウナ詳細情報の追加、削除における実装はまた後日

--- resources/js/app.js
+++ resources/js/app.js
@@ -84,7 +84,7 @@ $(function(){
                     <textarea name="note[]" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                 </td>
                 <td class="px-4 py-3">
-                    <button type="button" class="remove-row bg-red-500 text-white px-2 py-1 rounded">削除</button>
+                    <button type="button" class="remove-row bg-red-500 text-white px-4 py-2 rounded">削除</button>
                 </td>
             </tr>
         `;
--- resources/views/admin/saunas/edit.blade.php
+++ resources/views/admin/saunas/edit.blade.php
@@ -12,7 +12,7 @@
                         <div class="p-2 w-full">
                             <div class="relative">
                                 <label for="name" class="leading-7 text-sm text-gray-600">施設名</label>
-                                <input type="text" id="name" name="name" value="{{ is_null(old('name')) ? $saunaFacility->name : old('name') }}" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                <input type="text" id="name" name="name" value="{{ old('name', $saunaFacility->name)}}" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                             </div>
                             {{-- 施設名のエラーメッセージ --}}
                             @error('name')
@@ -26,7 +26,8 @@
                                     type="text"
                                     id="postal_code"
                                     name="postal_code"
-                                    value="{{ is_null(old('postal_code')) ? $saunaFacility->postal_code  : old('postal_code') }}"
+                                    value="{{ old('postal_code', $saunaFacility->postal_code) }}"
+                                    {{-- value="{{ is_null(old('postal_code')) ? $saunaFacility->postal_code  : old('postal_code') }}" --}}
                                     class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                                 >
                             </div>
@@ -42,7 +43,7 @@
                                     type="text"
                                     id="prefecture"
                                     name="prefecture"
-                                    value="{{ is_null(old('prefecture')) ? $saunaFacility->prefecture : old('prefecture') }}"
+                                    value="{{ old('prefecture',$saunaFacility->prefecture )}}"
                                     class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                                 >
                             </div>
@@ -55,7 +56,7 @@
                             <div class="relative">
                                 <label for="city" class="leading-7 text-sm text-gray-600">市区町村</label>
                                 <textarea id="city" name="city" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                                >{{ is_null(old('city')) ? $saunaFacility->city : old('city') }}</textarea>
+                                >{{ old('city', $saunaFacility->city) }}</textarea>
                             </div>
                             {{-- 市区町村のエラーメッセージ --}}
                             @error('city')
@@ -66,14 +67,14 @@
                             <div class="relative">
                                 <label for="address" class="leading-7 text-sm text-gray-600">それ以降の住所</label>
                                 <textarea id="address" name="address" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                                >{{ is_null(old('address')) ? $saunaFacility->address : old('address') }}</textarea>
+                                >{{ old('address', $saunaFacility->address)}}</textarea>
                             </div>
                         </div>
                         <div class="p-2 w-full">
                             <div class="relative">
                                 <label for="access" class="leading-7 text-sm text-gray-600">アクセス</label>
                                 <textarea id="access" name="access" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                                >{{ is_null(old('access')) ? $saunaFacility->access : old('access') }}</textarea>
+                                >{{ old('access',$saunaFacility->access ) }}</textarea>
                             </div>
                             {{-- アクセスのエラーメッセージ --}}
                             @error('access')
@@ -84,14 +85,14 @@
                             <div class="relative">
                                 <label for="location_map" class="leading-7 text-sm text-gray-600">地図</label>
                                 <textarea id="location_map" name="location_map" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                                >{{ is_null(old('location_map')) ? $saunaFacility->location_map : old('location_map') }}</textarea>
+                                >{{ old('location_map', $saunaFacility->location_map) }}</textarea>
                             </div>
                         </div>
                         <div class="p-2 w-full">
                             <div class="relative">
                                 <label for="price" class="leading-7 text-sm text-gray-600">料金</label>
                                 <textarea id="price" name="price" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                                >{{ is_null(old('price')) ? $saunaFacility->price : old('price') }}</textarea>
+                                >{{ old('price', $saunaFacility->price) }}</textarea>
                             </div>
                             {{-- 料金のエラーメッセージ --}}
                             @error('price')
@@ -101,7 +102,7 @@
                         <div class="p-2 w-full">
                             <div class="relative">
                                 <label for="image_path" class="leading-7 text-sm text-gray-600">画像</label>
-                                <input type="file" id="image_path" name="image_path" value="{{ is_null(old('image_path')) ? $saunaFacility->image_path : old('image_path')}}" class="col-span-2  p-2 rounded-lg">
+                                <input type="file" id="image_path" name="image_path" value="{{ old('image_path', $saunaFacility->image_path)}}" class="col-span-2  p-2 rounded-lg">
                                 @if ($saunaFacility->image_path)
                                     <img src="{{ asset('storage/'.$saunaFacility->image_path) }}" alt="サウナ画像" class="mt-2 h-32">
                                 @endif
@@ -133,10 +134,14 @@
                                         <tr>
                                             <td class="px-4 py-3">
                                                 <select name="sauna_type[]" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                                                    <option value="1" {{ $sauna->type == '1' ? 'selected' : '' }}>乾式サウナ（ドライサウナ）</option>
-                                                    <option value="2" {{ $sauna->type == '2' ? 'selected' : '' }}>湿式サウナ（スチームサウナ）</option>
-                                                    <option value="3" {{ $sauna->type == '3' ? 'selected' : '' }}>湿式サウナ（ミストサウナ）</option>
-                                                    <option value="4" {{ $sauna->type == '4' ? 'selected' : '' }}>ロウリュサウナ</option>
+                                                    @php
+                                                        $saunaType = old('sauna_type.'.$i) ?? $sauna->type;
+                                                    @endphp
+
+                                                    <option value="1" {{ $saunaType == '1' ? 'selected' : '' }}>乾式サウナ（ドライサウナ）</option>
+                                                    <option value="2" {{ $saunaType == '2' ? 'selected' : '' }}>湿式サウナ（スチームサウナ）</option>
+                                                    <option value="3" {{ $saunaType == '3' ? 'selected' : '' }}>湿式サウナ（ミストサウナ）</option>
+                                                    <option value="4" {{ $saunaType == '4' ? 'selected' : '' }}>ロウリュサウナ</option>
                                                 </select>
                                                 <!-- Validation Error for sauna_type -->
                                                 @error('sauna_type.' . $i)
@@ -144,7 +149,7 @@
                                                 @enderror
                                             </td>
                                             <td class="px-4 py-3">
-                                                <input type="text" name="temperature[]" value="{{ $sauna->temperature }}" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                                <input type="text" name="temperature[]" value="{{ old('temperature.' . $i, $sauna->temperature)  }}" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                                 <!-- Validation Error for temperature -->
                                                 @error('temperature.' . $i)
                                                     <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
@@ -152,7 +157,7 @@
                                             </td>
                                             <td class="px-4 py-3">
                                                 <textarea name="note[]" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                                                >{{ $sauna->note }}</textarea>
+                                                >{{ old('note.' . $i, $sauna->note) }}</textarea>
                                                 <!-- Validation Error for note -->
                                                 @error('note.' . $i)
                                                     <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
@@ -171,7 +176,7 @@
                             <div class="relative">
                                 <label for="saunaComment" class="leading-7 text-sm text-gray-600">サウナの備考</label>
                                 <textarea id="saunaComment" name="saunaComment" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                                >{{ is_null(old('saunaComment')) ? $saunaFacility->sauna_comment : old('saunaComment') }}</textarea>
+                                >{{ old('saunaComment', $saunaFacility->sauna_comment) }}</textarea>
                             </div>
                             {{-- サウナ備考のエラーメッセージ --}}
                             @error('saunaComment')
@@ -208,7 +213,7 @@
                             <div class="relative">
                                 <label for="loylyComment" class="leading-7 text-sm text-gray-600">ロウリュの備考</label>
                                 <textarea id="loylyComment" name="loylyComment" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                                >{{ is_null(old('loylyComment')) ? $saunaFacility->loyly_comment : old('loylyComment') }}</textarea>
+                                >{{ old('loylyComment', $saunaFacility->loyly_comment) }}</textarea>
                             </div>
                             {{-- ロウリュ備考のエラーメッセージ --}}
                             @error('loylyComment')
@@ -245,7 +250,7 @@
                             <div class="relative">
                                 <label for="waterbathComment" class="leading-7 text-sm text-gray-600">水風呂の備考</label>
                                 <textarea id="waterbathComment" name="waterbathComment" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                                >{{ is_null(old('waterbathComment')) ? $saunaFacility->water_bath_comment : old('waterbathComment') }}</textarea>
+                                >{{ old('waterbathComment', $saunaFacility->water_bath_comment) }}</textarea>
                             </div>
                             {{-- 水風呂備考のエラーメッセージ --}}
                             @error('waterbathComment')
@@ -308,7 +313,7 @@
                             <div class="relative">
                                 <label for="chairComment" class="leading-7 text-sm text-gray-600">ととのい椅子の備考</label>
                                 <textarea id="chairComment" name="chairComment" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                                >{{ is_null(old('chairComment')) ? $saunaFacility->chair_comment : old('chairComment') }}</textarea>
+                                >{{ old('chairComment', $saunaFacility->chair_comment) }}</textarea>
                             </div>
                             {{-- ととのい椅子備考のエラーメッセージ --}}
                             @error('chairComment')
